7decd492d0c152ac261ef4e24e5ee99ef7e3c821
Daniel Nkhoma <danielnkhoma@gmail.com>
2015-11-11 13:16:10 +0200

    Initial Commit

---
+++ src/main/java/zw/hitrac/hwo/HwoApplication.java
@@ -0,0 +1,17 @@
+package zw.hitrac.hwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Tonderai Ndangana
+ */
+@SpringBootApplication
+public class HwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/zw/hitrac/hwo/controller/Home.java
@@ -0,0 +1,17 @@
+package zw.hitrac.hwo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Tonderai Ndangana
+ */
+@Controller
+public class Home {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
